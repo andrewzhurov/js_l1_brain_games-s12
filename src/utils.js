1c0acf97ca91be96813a9e73d799cf96c8b2945f
Andrew <brownmoose3q@gmail.com>
2017-01-06 14:34:46 +0300

    one last

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
-export const ask = askStr => readlineSync.question(`${askStr} \n>`)	
+export const ask = askStr => readlineSync.question(`${askStr} \n>`);
